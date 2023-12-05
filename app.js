18cfb655cb1ac1434c867e945626b3c7f129b8b9
pranavaselva <pranava.m@kalvium.community>
2023-12-05 17:11:39 +0530

    completed

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 // Iteration 1: Making the play button in the index.html functional.
 // Description: When the play button is clicked the game.html page should be opened
+let playAgaianBtn = document.getElementById('play-button');
+playAgaianBtn.addEventListener('click', function () {
+  window.location.href = 'game.html';
+});
